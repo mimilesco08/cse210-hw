693419a8f2d9fca465c0a066fcb77aaae945eb8b
mimilesco08 <eescobar@byupathway.edu>
2025-09-12 19:18:41 +0800

    Finished Activity 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+        int lastDigit = percent % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // Handle special cases
+        if (letter == "A" && sign == "+")
+        {
+            sign = ""; // No A+
+        }
+        if (letter == "F")
+        {
+            sign = ""; // No F+ or F-
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+
+
+
     }
+
 }
